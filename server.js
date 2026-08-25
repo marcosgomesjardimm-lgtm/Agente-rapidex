b392f72cf869fe646e7dab0a60dd8168bd2ae107
marcosgomesjardimm-lgtm <marcosgomesjardimm@gmail.com>
2026-08-25 19:57:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const axios = require('axios');
+cons import express from 'express';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
